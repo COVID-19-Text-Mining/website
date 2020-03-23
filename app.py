5bf60c49c9854b4d5da35c155cb2d8a29144a1d8
jdagdelen <jjdagdelen@gmail.com>
2020-03-23 02:50:10 -0700

    Prototype working. Shows Google Form data.

---
+++ app.py
@@ -0,0 +1,7 @@
+
+from covidscholar_web.app import app
+
+server = app.server
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+from setuptools import find_packages, setup
+
+setup(
+    name="covidscholar_web",
+    version="1.0.0",
+    description="Search tool for COVID-19 research.",
+    long_description="",
+    author="COVID-19 Scholar Development Team",
+    author_email="jdagdelen@lbl.gov",
+    url="https://github.com/COVID-19-Text-Mining/website",
+    license=license,
+    packages=find_packages(),
+)
